1f2c5808a39e320806abf3a8a21ac515b56303b9
afigmatrix <122856539+afigmatrix@users.noreply.github.com>
2024-10-02 00:42:33 +0400

    Add files via upload
    project added

---
+++ Matrix1141EF/Matrix1141EF/Data/Entity/Student.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Matrix1141EF.Data.Entity
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public DateTime BirthDate{ get; set; }
+    }
+}
---
+++ Matrix1141EF/Matrix1141EF/Model/DTO/StudentCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Matrix1141EF.Model.DTO
+{
+    public class StudentCreateDTO
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
