798a1d65d9d4066ff6e59dd39805b4a2309d85f1
afiqmehdizada <afig.matrix@gmail.com>
2024-10-03 15:50:26 +0400

    test

--- Matrix1141EF/Matrix1141EF/Model/DTO/StudentCreateDTO.cs
+++ Matrix1141EF/Matrix1141EF/Model/DTO/StudentCreateDTO.cs
@@ -5,7 +5,7 @@ namespace Matrix1141EF.Model.DTO
     public class StudentCreateDTO
     {
         public string Name { get; set; }
-        public int? Age { get; set; }
+        public int Age { get; set; }
         public DateTime BirthDate { get; set; }
     }
 }
