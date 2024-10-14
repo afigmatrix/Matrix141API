b0fcad08e1506afdb6fc8dca4dc4e2975e9c529a
Turkay Novruzlu <tnovruzlu7682@gmail.com>
2024-10-14 20:51:09 +0400

    second commit

---
+++ Matrix1141EF/Matrix1141EF/Model/DTO/ProductDeleteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Matrix1141EF.Model.DTO
+{
+    public class ProductDeleteDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string CategoryName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal Currency { get; set; }
+    }
+}
